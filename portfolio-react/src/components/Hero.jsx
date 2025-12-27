39b3f6024ef0a0297e297919a50a77dc3ca6d292
Davy Jean-Charles <dave.jc971@gmail.com>
2025-12-27 13:59:53 -0400

    Start Home with Hero part

---
+++ portfolio-react/src/components/Hero.jsx
@@ -0,0 +1,15 @@
+function Hero() {
+  return (
+    <section className="hero">
+      <div className="container text-center">
+        <h1 className="hero-title">John Doe</h1>
+        <p className="hero-subtitle">Développeur Web</p>
+        <button className="btn btn-primary">
+          En savoir plus
+        </button>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
