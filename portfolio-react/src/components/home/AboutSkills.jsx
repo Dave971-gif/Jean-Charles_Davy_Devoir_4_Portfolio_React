3f7a4276146a273b430f228f142552f7960d020a
Davy Jean-Charles <dave.jc971@gmail.com>
2026-01-07 19:53:15 -0400

    Modifs done for extra large screens, ending pages, verifying some forgotten things and bugs to improve pages, Porfolio Done. END

--- portfolio-react/src/components/home/AboutSkills.jsx
+++ portfolio-react/src/components/home/AboutSkills.jsx
@@ -4,7 +4,7 @@ function AboutSkills() {
       <div className="about container-fluid py-5 col-lg-6 col-md-12 mb-4">
         <h2 className="mb-4">A propos</h2>
 
-        <img src="/images/john-doe-about.jpg" alt="A propos de moi" className="about-image mb-3 mx-auto d-block img-fluid" />
+        <img src="images/john-doe-about.jpg" alt="A propos de moi" className="about-image mb-3 mx-auto d-block img-fluid" />
         
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
